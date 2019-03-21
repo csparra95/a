3ad82e102ef91a7f7d40e521e57b8750ae91414d
csparra95 <48566625+csparra95@users.noreply.github.com>
2019-03-21 16:40:56 -0600

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ConsoleApp10
+{
+
+    public class GeneralValidator
+    {
+        
+        bool Success;
+        private ResultMessage ValidateString(string str, char[] specialChars, Func<char, bool> fn, bool acceptSpace = true)
+        {
+            ResultMessage result = new ResultMessage();
+            List<char> tempStr = str.ToList();
+
+            if (acceptSpace)
+            {
+                tempStr.RemoveAll(c => c == ' ');
+            }
+
+            if (specialChars != null)
+            {
+                tempStr.RemoveAll(c => specialChars.Contains(c));
+            }
+
+            Success = tempStr.All(c => fn(c));
+            
+            Console.WriteLine(result);
+            Console.ReadKey();
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
